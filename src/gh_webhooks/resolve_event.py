f2cb9f2e9740743d4794df62321d6c721faacaeb
Kevin Duff <kevinkelduff@gmail.com>
2022-06-25 18:28:16 +0100

    Dynamic version from introspection

--- src/gh_webhooks/resolve_event.py
+++ src/gh_webhooks/resolve_event.py
@@ -1,134 +1,26 @@
+import inspect
 import logging
-from typing import Any, Dict, Optional, Type
-from gh_webhooks.exceptions import NoMatchingModel
+from typing import Any, Dict, Type
 
-from gh_webhooks.types import (
-    BranchProtectionRuleEvent,
-    CheckRunEvent,
-    CheckSuiteEvent,
-    CodeScanningAlertEvent,
-    CommitCommentEvent,
-    CreateEvent,
-    DeleteEvent,
-    DeployKeyEvent,
-    DeploymentEvent,
-    DeploymentStatusEvent,
-    DiscussionEvent,
-    DiscussionCommentEvent,
-    ForkEvent,
-    GithubAppAuthorizationEvent,
-    GollumEvent,
-    InstallationEvent,
-    InstallationRepositoriesEvent,
-    IssueCommentEvent,
-    IssuesEvent,
-    LabelEvent,
-    MarketplacePurchaseEvent,
-    MemberEvent,
-    MembershipEvent,
-    MetaEvent,
-    MilestoneEvent,
-    Model,
-    OrgBlockEvent,
-    OrganizationEvent,
-    PackageEvent,
-    PageBuildEvent,
-    PingEvent,
-    ProjectEvent,
-    ProjectCardEvent,
-    ProjectColumnEvent,
-    ProjectsV2ItemEvent,
-    PublicEvent,
-    PullRequestEvent,
-    PullRequestReviewEvent,
-    PullRequestReviewCommentEvent,
-    PullRequestReviewThreadEvent,
-    PushEvent,
-    ReleaseEvent,
-    RepositoryEvent,
-    RepositoryDispatchEvent,
-    RepositoryImportEvent,
-    RepositoryVulnerabilityAlertEvent,
-    SecretScanningAlertEvent,
-    SecurityAdvisoryEvent,
-    SponsorshipEvent,
-    StarEvent,
-    StatusEvent,
-    TeamEvent,
-    TeamAddEvent,
-    WatchEvent,
-    WorkflowDispatchEvent,
-    WorkflowJobEvent,
-    WorkflowRunEvent,
-)
+import stringcase
+
+import gh_webhooks.types as generated_types
+from gh_webhooks.exceptions import NoMatchingModel
+from gh_webhooks.types import Model
 
 logger = logging.getLogger(__name__)
 
-_cls_mapping = {
-    "branch_protection_rule": BranchProtectionRuleEvent,
-    "check_run": CheckRunEvent,
-    "check_suite": CheckSuiteEvent,
-    "code_scanning_alert": CodeScanningAlertEvent,
-    "commit_comment": CommitCommentEvent,
-    "create": CreateEvent,
-    "delete": DeleteEvent,
-    "deploy_key": DeployKeyEvent,
-    "deployment": DeploymentEvent,
-    "deployment_status": DeploymentStatusEvent,
-    "discussion": DiscussionEvent,
-    "discussion_comment": DiscussionCommentEvent,
-    "fork": ForkEvent,
-    "github_app_authorization": GithubAppAuthorizationEvent,
-    "gollum": GollumEvent,
-    "installation": InstallationEvent,
-    "installation_repositories": InstallationRepositoriesEvent,
-    "issue_comment": IssueCommentEvent,
-    "issues": IssuesEvent,
-    "label": LabelEvent,
-    "marketplace_purchase": MarketplacePurchaseEvent,
-    "member": MemberEvent,
-    "membership": MembershipEvent,
-    "meta": MetaEvent,
-    "milestone": MilestoneEvent,
-    "org_block": OrgBlockEvent,
-    "organization": OrganizationEvent,
-    "package": PackageEvent,
-    "page_build": PageBuildEvent,
-    "ping": PingEvent,
-    "project": ProjectEvent,
-    "project_card": ProjectCardEvent,
-    "project_column": ProjectColumnEvent,
-    "projects_v2_item": ProjectsV2ItemEvent,
-    "public": PublicEvent,
-    "pull_request": PullRequestEvent,
-    "pull_request_review": PullRequestReviewEvent,
-    "pull_request_review_comment": PullRequestReviewCommentEvent,
-    "pull_request_review_thread": PullRequestReviewThreadEvent,
-    "push": PushEvent,
-    "release": ReleaseEvent,
-    "repository": RepositoryEvent,
-    "repository_dispatch": RepositoryDispatchEvent,
-    "repository_import": RepositoryImportEvent,
-    "repository_vulnerability_alert": RepositoryVulnerabilityAlertEvent,
-    "secret_scanning_alert": SecretScanningAlertEvent,
-    "security_advisory": SecurityAdvisoryEvent,
-    "sponsorship": SponsorshipEvent,
-    "star": StarEvent,
-    "status": StatusEvent,
-    "team": TeamEvent,
-    "team_add": TeamAddEvent,
-    "watch": WatchEvent,
-    "workflow_dispatch": WorkflowDispatchEvent,
-    "workflow_job": WorkflowJobEvent,
-    "workflow_run": WorkflowRunEvent,
-}
-
 
 def _get_cls(kind: str) -> Type[Model]:
-    # TODO: make this dynamic by constructing the event name and finding it
-    cls = _cls_mapping.get(kind)
-    if not kind:
-        raise NoMatchingModel(f"No matching event kind")
+    class_name = stringcase.pascalcase(kind) + "Event"
+    matches = [
+        obj for name, obj in inspect.getmembers(generated_types) if name == class_name
+    ]
+
+    if len(matches) != 1:
+        raise NoMatchingModel(f"No event matching kind {kind!r}")
+
+    (cls,) = matches
     return cls
 
 
--- tests/integration/test_github_doc_examples.py
+++ tests/integration/test_github_doc_examples.py
@@ -1,5 +1,3 @@
-from typing import Any, Dict, List
-
 import pytest
 
 from gh_webhooks import GhWebhookEventHandler
